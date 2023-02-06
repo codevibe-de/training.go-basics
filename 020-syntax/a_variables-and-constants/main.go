5f6a6d7b1efe32ee1a58c28638f74291cfdc7371
Thomas Auinger <thomas@auinger.de>
2023-02-06 16:45:29 +0100

    solution 020-a

--- 020-syntax/a_variables-and-constants/main.go
+++ 020-syntax/a_variables-and-constants/main.go
@@ -4,4 +4,36 @@ import "fmt"
 
 func main() {
 	fmt.Println("Variables & constants:")
+	constants()
+	iotaConsts()
+}
+
+func constants() {
+	const x = 254
+	const y int = 1
+	const (
+		a = "Hello"
+		b = "World"
+	)
+	fmt.Println(x, y, a, b)
+	fmt.Printf("Constant `x` has type ´%T`\n", x)
+	var d8 uint8 = x
+	var d16 int16 = x
+	var d32 int32 = x
+	var d64 int64 = x
+	fmt.Println(d8, d16, d32, d64)
+
+	var v8 int8 = 1
+	//var v16 int16 = v8 // doesn't compile
+	fmt.Println(v8)
+}
+
+func iotaConsts() {
+	const (
+		KB = (1 << (10 * iota))
+		MB
+		GB
+		TB
+	)
+	fmt.Printf("1 TB has %d bytes\n", TB)
 }
