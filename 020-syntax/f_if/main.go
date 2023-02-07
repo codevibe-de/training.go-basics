645fae34b89ab08a35e6e5cd5df3d83affa98a8c
Thomas Auinger <thomas@auinger.de>
2023-02-07 11:54:26 +0100

    solution if

--- 020-syntax/f_if/main.go
+++ 020-syntax/f_if/main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("If-Statements:")
+	numbers := [10]int{2, 22, 333, 4, -123, 888, 4, 8080}
+	min, max := findMinMax(numbers)
+	fmt.Printf("min=%d, max=%d", min, max)
+}
+
+func findMinMax(arr [10]int) (int, int) {
+	min, max := arr[0], arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+		if arr[i] < min {
+			min = arr[i]
+		}
+	}
+	return min, max
 }
