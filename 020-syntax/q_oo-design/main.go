4659c17c057b9aa02247ce71bc0bb4f67eb110d3
Thomas Auinger <thomas@auinger.de>
2023-02-08 10:25:07 +0100

    solutn ood

--- 020-syntax/q_oo-design/main.go
+++ 020-syntax/q_oo-design/main.go
@@ -17,5 +17,50 @@ func main() {
 		{"423-001", "I. Wellmann", "Random House"},
 		{"193-753", "T. Müller", "Wort und Bild Verlag"},
 	}
-	fmt.Println(books)
+
+	shelf := bookshelf{}
+	for _, b := range books {
+		shelf.add(b)
+	}
+
+	fmt.Println("Get one book:")
+	fmt.Println(shelf.forIsbn("123-456"))
+
+	fmt.Println("\nGet all books:")
+	fmt.Println(shelf.all())
+}
+
+type bookshelf struct {
+	booksByIsbn map[string]book
+}
+
+func (bs *bookshelf) add(b book) error {
+	bs.init()
+	_, exists := bs.booksByIsbn[b.isbn]
+	if exists {
+		return fmt.Errorf("This bookshelf contains already a book with ISBN `%s`", b.isbn)
+	} else {
+		bs.booksByIsbn[b.isbn] = b
+		return nil
+	}
+}
+
+func (bs *bookshelf) forIsbn(isbn string) book {
+	bs.init()
+	return bs.booksByIsbn[isbn]
+}
+
+func (bs *bookshelf) all() []book {
+	bs.init()
+	booksSlice := make([]book, 0, len(bs.booksByIsbn))
+	for _, b := range bs.booksByIsbn {
+		booksSlice = append(booksSlice, b)
+	}
+	return booksSlice
+}
+
+func (bs *bookshelf) init() {
+	if bs.booksByIsbn == nil {
+		bs.booksByIsbn = make(map[string]book)
+	}
 }
