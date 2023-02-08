3116b8c734499ec3994b14dde99c226435ab0100
Thomas Auinger <thomas@auinger.de>
2023-02-08 07:32:46 +0100

    sltn variadic + funcvars

--- 020-syntax/j_variadic-functions/main.go
+++ 020-syntax/j_variadic-functions/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Variadic Functions:")
+	a, b, c, d := 10, 2, 0, -42
+	doubleInts(&a, &b, &c, &d)
+	fmt.Println(a, b, c, d)
+}
+
+func doubleInts(ints ...*int) {
+	for i := range ints {
+		*ints[i] = *ints[i] * 2
+	}
 }
--- 020-syntax/l_function-vars/main.go
+++ 020-syntax/l_function-vars/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type operatorFunc func(int, int) int
+
 func main() {
 	fmt.Println("1st class Functions:")
+
+	var sumOperator operatorFunc = func(a, b int) int { return a + b }
+	var minusOperator operatorFunc = func(a, b int) int { return a - b }
+	var multiplyOperator operatorFunc = func(a, b int) int { return a * b }
+
+	oneMinusOne := minusOperator(1, 1)
+	fmt.Println(oneMinusOne)
+
+	twoPlusFourTimesTen := multiplyOperator(sumOperator(2, 4), 10)
+	fmt.Println(twoPlusFourTimesTen)
 }
