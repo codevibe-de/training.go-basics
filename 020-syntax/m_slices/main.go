dbc8af76d6d990fbb98d9e9d9bb2facd0f63c6fa
Thomas Auinger <thomas@auinger.de>
2023-02-08 07:38:48 +0100

    stack wip

--- 020-syntax/m_slices/main.go
+++ 020-syntax/m_slices/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type stack []string
+
 func main() {
 	fmt.Println("Slices:")
+	myStack := make(stack, 0)
+	myStack = push(myStack, "first")
+	myStack = push(myStack, "second")
+	myStack = pop(myStack)
+	myStack = push(myStack, "third")
+	fmt.Println(myStack)
+}
+
+func push(st stack, item string) stack {
+	return append(st, item)
+}
+
+func pop(st stack) (stack, string) {
+	item := st[len(st)-1]
+	return st[:len(st)-1]
 }
