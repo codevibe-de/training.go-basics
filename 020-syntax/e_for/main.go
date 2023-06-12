7e043b1852a6051fa551ccada0fca007d012c24d
Thomas Auinger <thomas@auinger.de>
2023-06-12 21:47:08 +0200

    sltns 010-a and 020-e

--- 020-syntax/e_for/main.go
+++ 020-syntax/e_for/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// https://www.frontlinesedutech.com/blog/why-computers-screw-up-doing-math-on-floating-point-numbers
 func main() {
 	fmt.Println("For-Loops:")
+	total := sum([]float32{0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1})
+	fmt.Println(total)
+}
+
+func sum(numbers []float32) float32 {
+	var result float32 = 0.0
+	for _, v := range numbers {
+		result += v
+	}
+	return result
 }
