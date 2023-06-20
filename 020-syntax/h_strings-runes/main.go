7c6a4e9f382732229c513f23d012651dc5084561
Thomas Auinger <thomas@auinger.de>
2023-06-20 11:02:41 +0200

    slnt 020-h extended by kebab and improved other

--- 020-syntax/h_strings-runes/main.go
+++ 020-syntax/h_strings-runes/main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	fmt.Println("Strings and Runes:")
 
-	s := "Heute lerne ich Go"
-	words := strings.Split(s, " ")
+	s := "HeuTe lerne ich Go"
+	words := strings.Split(strings.ToLower(s), " ")
 	for i, w := range words {
 		runes := []rune(w)
 		runes[0] = unicode.ToUpper(runes[0])
@@ -18,4 +18,8 @@ func main() {
 	}
 	camelCase := strings.Join(words, "")
 	fmt.Println(camelCase)
+
+	words = strings.Split(strings.ToLower(s), " ")
+	kebabCase := strings.Join(words, "-")
+	fmt.Println(kebabCase)
 }
