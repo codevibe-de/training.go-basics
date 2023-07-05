80f0bd600ae013d71ab4d68d21eee6f45fed399d
Thomas Auinger <thomas@auinger.de>
2023-07-05 09:02:05 +0200

    cleaned demo.go

--- demo.go
+++ demo.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	m := make(map[string]bool)
-	m["hey"] = true
-	m["ho"] = false
-	fmt.Println(m, len(m))
-	delete(m, "ho")
-	fmt.Println(m, len(m))
-
-	v := m["blah"]
-	_, e := m["blah"]
 }
