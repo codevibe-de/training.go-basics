8e4debde01d2cf7d996496a82cb895c1debf5806
Thomas Auinger <thomas@auinger.de>
2023-07-10 10:46:37 +0200

    040 simplified fib sltn

--- 040_testing/fib.go
+++ 040_testing/fib.go
@@ -1,20 +1,16 @@
 package main
 
 var (
-	prev     = 0
-	prevPrev = 0
+	prev     int
+	prevPrev int
 )
 
 func Reset() {
-	prev, prevPrev = 0, 0
+	prev, prevPrev = 0, 1
 }
 
-// NextFib returns the next Fibonacci number
 func NextFib() int {
 	res := prev + prevPrev
-	if res == 0 {
-		res = 1
-	}
 	prev, prevPrev = res, prev
 	return res
 }
